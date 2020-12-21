e71a9f161776ac712aadea17317d9d48ca52a94b
burhansaydin <burhasaydin@gmail.com>
2020-12-21 20:38:02 +0200

    -to check the your position is within +5 or -5 degrees of the ISS position position_check function created -Then if the sun is set and position function true, send me an email to tell me to look up. -run the code every 60 seconds with time.sleep function

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+
+MY_LAT = 37.983810 # Your latitude
+MY_LONG = 23.727539 # Your longitude
+password = "qetuo123"
+my_email = "qazxswqaz04@gmail.com"
+
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -12,6 +18,8 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
 #Your position is within +5 or -5 degrees of the ISS position.
+def position_check():
+    return (abs(MY_LAT-iss_latitude) <= 5) and (abs(MY_LONG-iss_longitude) <= 5)
 
 
 parameters = {
@@ -27,11 +35,20 @@ sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
+hour = time_now.hour
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+
+while True:
+    time.sleep(60)
+    if position_check() and sunset <= hour:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs= my_email,
+                msg="Subject:Look Up \n\n The ISS is above you in the sky."
+            )
 
 
 
